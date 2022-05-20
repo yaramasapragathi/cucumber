c8edba07ee8ea793a3ad2d896bc466a8af83c246
yaramasapragathi <pragathiyaramasa39@gmail.com>
2022-05-20 19:05:13 +0530

    my second one

---
+++ src/test/java/testrunner/runner.java
@@ -0,0 +1,19 @@
+package testrunner;
+
+
+import org.junit.runner.RunWith;
+
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features="C:\\Users\\HP\\eclipse-workspace\\hybcum\\features",
+glue="stepdefinitions",monochrome=true,dryRun=false,plugin={"pretty","html:test-output"
+		})
+
+public class runner {
+	
+}
